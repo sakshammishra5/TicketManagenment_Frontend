68ba3ed9e70318827472d15744ee717f7f096653
saksham <saksham7dev@gmail.com>
2025-12-02 11:22:23 +0000

    .

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 // http://localhost:5000
 // https://ticketmanagenment-backend.onrender.com
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://ticketmanagenment-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
